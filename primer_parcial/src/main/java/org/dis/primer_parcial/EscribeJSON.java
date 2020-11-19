f566ab93e2f828f1022bdd9d216bd54ccc7e35c1
Imanol <imanol24garcia@gmail.com>
2020-11-19 21:31:56 +0100

    CSV v2

--- primer_parcial/src/main/java/org/dis/primer_parcial/EscribeJSON.java
+++ primer_parcial/src/main/java/org/dis/primer_parcial/EscribeJSON.java
@@ -1,10 +1,22 @@
 package org.dis.primer_parcial;
+
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 //Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
 
 public class EscribeJSON {
 
     public static void main(String[] args) {
-    
+    	
+    	
+    	ArrayList<golfPlayer> jugadores = new ArrayList<golPlayer>();
+    	
+    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonStr = gson.toJson(jugadores);
+        System.out.println(jsonStr);
     }
     
  }
--- primer_parcial/src/main/java/org/dis/primer_parcial/LeerCSV.java
+++ primer_parcial/src/main/java/org/dis/primer_parcial/LeerCSV.java
@@ -1,10 +1,12 @@
 // lectura del fichero csv mediante el objeto plano y su carga en una lista de objetos de este tipo
 package org.dis.primer_parcial;
-import java.util.*;
 
-
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.*; 
+import java.util.*; 
+  
+import com.opencsv.CSVReader; 
+import com.opencsv.bean.CsvToBean; 
+import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 
 // Otros imports ...
 
@@ -12,7 +14,37 @@ public class LeerCSV {
 
     public ArrayList<golfPlayer> parseaCSV(String path) {
         
-        ArrayList<golfPlayer> players = new CsvToBeanBuilder(FileReader("../../../../../../../best-golf-players.csv"))
+    	Map<String, String> mapping = new 
+                HashMap<String, String>();
+    	
+    	mapping.put("Posicion", "Posicion"); 
+        mapping.put("Nombre", "Nombre"); 
+        mapping.put("Apellido", "Apellido"); 
+        mapping.put("Pais", "Pais"); 
+        mapping.put("Edad", "Edad");
+        
+        
+        HeaderColumnNameTranslateMappingStrategy<golfPlayer> strategy = 
+                new HeaderColumnNameTranslateMappingStrategy<golfPlayer>(); 
+           strategy.setType(golfPlayer.class); 
+           strategy.setColumnMapping(mapping);
+           
+           CSVReader csvReader = null; 
+           try { 
+               csvReader = new CSVReader(new FileReader 
+               ("D:\\EclipseWorkSpace\\CSVOperations\\StudentData.csv")); 
+           } 
+           catch (FileNotFoundException e) { 
+     
+               // TODO Auto-generated catch block 
+               e.printStackTrace(); 
+           } 
+           CsvToBean csvToBean = new CsvToBean(); 
+     
+           // call the parse method of CsvToBean 
+           // pass strategy, csvReader to parse method 
+           List<golfPlayer> list = csvToBean.parse(strategy, csvReader); 
+     
         return players;
     }
 }
